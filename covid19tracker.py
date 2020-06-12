468579e13d664d692fa3f1b445be36c6e12264ea
Anshul Tripathi <anshul.3pathi@gmail.com>
2020-06-13 02:24:18 +0530

    converted values from string to integers

--- covid19tracker.py
+++ covid19tracker.py
@@ -9,10 +9,17 @@ driver.get("https://www.covid19india.org/")
 time.sleep(10)
 
 attributes = []
+values = []
 root = driver.find_element_by_id("root")
 level = root.find_element_by_class_name("Level")
 h5 = level.find_elements_by_tag_name("h5")
-for h in h5:
-    attributes.append(h.text)
-print(attributes)
+h1 = level.find_elements_by_tag_name("h1")
+for x, y in zip(h5, h1):
+    attributes.append(x.text)
+    values.append(y.text)
+
+for val in enumerate(values):
+    values[val[0]] = int(val[1].replace(',', ''))
+
+print(attributes, values)
 driver.quit()
